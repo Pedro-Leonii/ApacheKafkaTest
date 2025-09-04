35023763630653172a71b6ca25be46c68007916b
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-04 18:00:27 +0200

    refactor(docker): Sposta la creazione dei topic in uno script bash esterno

---
+++ topics-creation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+topics="server.metrics server.logs.application server.logs.access"
+bootstrap_servers="broker1:9092, broker2:9092, broker3:9092"
+partitions=1
+replication_factor=3
+min_insync_replicas=2
+
+sleep 5 
+
+for topic in $topics; do
+    /opt/kafka/bin/kafka-topics.sh --create \
+        --topic "$topic" \
+        --partitions $partitions \
+        --replication-factor $replication_factor \
+        --config min.insync.replicas=$min_insync_replicas \
+        --bootstrap-server $bootstrap_servers
+done
+
+
