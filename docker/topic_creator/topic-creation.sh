49d82d3a99103e3dea5c9614e40b8434b44c7659
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-09 12:13:54 +0200

    refactor(code): cambia il numero di partizioni per la creazione dei topic

--- docker/topic_creator/topic-creation.sh
+++ docker/topic_creator/topic-creation.sh
@@ -6,11 +6,10 @@ partitions=1
 replication_factor=3
 min_insync_replicas=2
 
-sleep 5 
-
 for topic in $topics; do
     /opt/kafka/bin/kafka-topics.sh --create \
         --topic "$topic" \
+        --if-not-exists \
         --partitions $partitions \
         --replication-factor $replication_factor \
         --config min.insync.replicas=$min_insync_replicas \
