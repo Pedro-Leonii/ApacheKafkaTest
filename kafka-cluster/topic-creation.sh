ab3e76262536da0c551a21b11a4d38d460046f9a
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-16 17:44:59 +0200

    feature(kafka-cluster): agggiunge configurazione limitazione risorse e funzionamento su rete locale

--- kafka-cluster/topic-creation.sh
+++ kafka-cluster/topic-creation.sh
@@ -2,9 +2,18 @@
 
 topics="server.metrics server.logs.application server.logs.access"
 bootstrap_servers="broker1:9092, broker2:9092, broker3:9092"
-partitions=6
+partitions=12
 replication_factor=3
 min_insync_replicas=2
+
+
+/opt/kafka/bin/kafka-topics.sh --create \
+        --topic _schemas \
+        --if-not-exists \
+        --partitions 1 \
+        --replication-factor 3 \
+        --config min.insync.replicas=3, cleanup.policy=compact \
+        --bootstrap-server $bootstrap_servers
 
 for topic in $topics; do
     /opt/kafka/bin/kafka-topics.sh --create \
