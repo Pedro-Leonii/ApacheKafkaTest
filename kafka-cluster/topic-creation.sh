60044118c936deb8059126991cbb81daaa96291d
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-17 14:33:11 +0200

    fix(topic-creation): semplifica copia e incolla dei topic

--- kafka-cluster/topic-creation.sh
+++ kafka-cluster/topic-creation.sh
@@ -1,26 +1,32 @@
-#!/bin/bash
-
-topics="server.metrics server.logs.application server.logs.access"
-bootstrap_servers="broker1:9092, broker2:9092, broker3:9092"
-partitions=12
-replication_factor=3
-min_insync_replicas=2
-
-
 /opt/kafka/bin/kafka-topics.sh --create \
         --topic _schemas \
         --if-not-exists \
         --partitions 1 \
         --replication-factor 3 \
-        --config min.insync.replicas=3, cleanup.policy=compact \
-        --bootstrap-server $bootstrap_servers
+        --config min.insync.replicas=3 \
+        --config cleanup.policy=compact \
+        --bootstrap-server broker1:9092, broker2:9092, broker3:9092
 
-for topic in $topics; do
-    /opt/kafka/bin/kafka-topics.sh --create \
-        --topic "$topic" \
+/opt/kafka/bin/kafka-topics.sh --create \
+        --topic server.metrics \
         --if-not-exists \
-        --partitions $partitions \
-        --replication-factor $replication_factor \
-        --config min.insync.replicas=$min_insync_replicas \
-        --bootstrap-server $bootstrap_servers
-done
+        --partitions 12 \
+        --replication-factor 3 \
+        --config min.insync.replicas=2 \
+        --bootstrap-server broker1:9092, broker2:9092, broker3:9092
+
+/opt/kafka/bin/kafka-topics.sh --create \
+        --topic server.logs.application \
+        --if-not-exists \
+        --partitions 12 \
+        --replication-factor 3 \
+        --config min.insync.replicas=2 \
+        --bootstrap-server broker1:9092, broker2:9092, broker3:9092
+
+/opt/kafka/bin/kafka-topics.sh --create \
+        --topic server.logs.access \
+        --if-not-exists \
+        --partitions 12 \
+        --replication-factor 3 \
+        --config min.insync.replicas=2 \
+        --bootstrap-server broker1:9092, broker2:9092, broker3:9092
