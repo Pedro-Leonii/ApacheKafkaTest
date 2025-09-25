6f465e6492af97395e30dfa813809983ccf9172d
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-25 15:15:49 +0200

    refactor(s3 worker): aggiunge link esterni verso il cluster, lo schema registry e s3

--- worker/s3/s3-connector.sh
+++ worker/s3/s3-connector.sh
@@ -1,40 +1,42 @@
-WORKER_ADDR=$1
-S3_URL=$2
+worker_addr=$1
+s3_host=s3
 
-CONNECTOR="s3-sink-connector"
-TOPICS="servers.logs.access, servers.logs.application, servers.metrics"
+topics=(servers.logs.access servers.logs.application servers.metrics)
 
-curl -i -X PUT -H "Accept:application/json"\
-        -H "Content-Type:application/json" http://$WORKER_ADDR/connectors/$CONNECTOR/config \
+for topic in ${topics[@]}; do 
+    connector="s3-${topic//./-}"
+    curl -i -X PUT -H "Accept:application/json"\
+        -H "Content-Type:application/json" "http://$worker_addr:8083/connectors/$connector/config" \
         -d  @- <<EOF
-        {
-            "connector.class": "io.confluent.connect.s3.S3SinkConnector",
-            
-            "tasks.max": 36,
-
-            "topics": "$TOPICS",
-            "topics.dir": "topics",
-            
-            "rotate.interval.ms": 3600000,
-            "flush.size": 1000,
-
-            "auto.register.schemas": false,
-            "use.latest.version": true,
-
-            "store.url": "$S3_URL",
-            "s3.bucket.name": "testbucket",
-            "storage.class": "io.confluent.connect.s3.storage.S3Storage",
-            "aws.access.key.id": "none",
-            "aws.secret.access.key": "none",
-
-            "partitioner.class": "io.confluent.connect.storage.partitioner.DailyPartitioner",
-            "locale": "it_IT",
-            "timestamp.extractor": "RecordField",
-            "timestamp.field": "generation_time",
-            "timezone": "UTC",
-
-            "format.class": "io.confluent.connect.s3.format.parquet.ParquetFormat",
-            "parquet.codec": "snappy"
-
-        }
+{
+    "connector.class": "io.confluent.connect.s3.S3SinkConnector",
     
+    "tasks.max": 12,
+
+    "topics": "$topic",
+    "topics.dir": "topics",
+    
+    "rotate.interval.ms": 3600000,
+    "flush.size": 1000,
+
+    "auto.register.schemas": false,
+    "use.latest.version": true,
+
+    "store.url": "http://$s3_host:9095",
+    "s3.bucket.name": "testbucket",
+    "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+    "aws.access.key.id": "none",
+    "aws.secret.access.key": "none",
+
+    "partitioner.class": "io.confluent.connect.storage.partitioner.DailyPartitioner",
+    "locale": "IT_it",
+    "timezone": "Europe/Rome",
+    "timestamp.extractor": "RecordField",
+    "timestamp.field": "generation_time",
+
+    "format.class": "io.confluent.connect.s3.format.parquet.ParquetFormat",
+    "parquet.codec": "snappy"
+
+}
+EOF
+done
