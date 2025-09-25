6853edf475a3cecd1ab0d46843573adfb30afb4b
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-25 15:13:00 +0200

    feature(timescaledb): aggiunge configurazione del container timescaledb

--- archiviazione/timescaledb/init.sql
+++ archiviazione/timescaledb/init.sql
@@ -1,42 +1,36 @@
-CREATE DATABASE logs;
+CREATE DATABASE kafka;
 
-\c logs
-
-CREATE TYPE http_method as ENUM ('GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH');
-
-CREATE TYPE http_status_code as ENUM ('100','101','102','103','200','201','202','203','204','205','206','207','208','226','300','301','302','303','304','305','306','307','308','400','401','402','403','404','405','406','407','408','409','410','411','412','413','414','415','416','417','418','421','422','423','424','425','426','428','429','431','451','500','501','502','503','504','505','506','507','508','510','511');
-
-CREATE TYPE log_severity as ENUM ('INFO', 'DEBUG', 'WARNING', 'ERROR');
+\c kafka
 
 CREATE DOMAIN unsigned_int AS integer CHECK (VALUE > 0);
 
-CREATE DOMAIN cpu_percentage AS numeric(3,2) CHECK (VALUE > 0);
+CREATE DOMAIN cpu_percentage AS int CHECK (VALUE > 0 AND VALUE < 100);
 
-CREATE TABLE servers_access_logs(
+CREATE TABLE servers_logs_access(
     server_id varchar(255) NOT NULL,
     generation_time timestamp NOT NULL,
-    user_ip cidr NOT NULL,
+    user_ip varchar(45) NOT NULL,
     username varchar(255),
     request_url varchar(1000),
-    request_method http_method NOT NULL,
-    response_status_code http_status_code NOT NULL,
+    request_method varchar(10) NOT NULL,
+    response_status_code unsigned_int NOT NULL,
     response_dim unsigned_int NOT NULL,
     user_agent varchar(300) NOT NULL,
     PRIMARY KEY (generation_time, server_id)
 )
 WITH(
-    tsdb.hypertable,
+    tsdb.hypertable=true,
     tsdb.partition_column='generation_time',
     tsdb.chunk_interval='1d',
-    tsdb.segmentedby='server_id'
+    tsdb.segmentby='server_id'
 );
 
-CALL add_columnstore_policy('server_application_logs', after => INTERVAL '1d');
+CALL add_columnstore_policy('servers_logs_access', after => INTERVAL '1d');
 
-CREATE TABLE servers_application_logs(
+CREATE TABLE servers_logs_application(
     server_id varchar(255) NOT NULL,
     generation_time timestamp NOT NULL,
-    severity severity NOT NULL,
+    severity varchar(10) NOT NULL,
     msg varchar(500) NOT NULL,
     source_thread varchar(100) NOT NULL,
     source_class varchar(100) NOT NULL,
@@ -44,13 +38,13 @@ CREATE TABLE servers_application_logs(
 
 )
 WITH(
-    tsdb.hypertable,
+    tsdb.hypertable=true,
     tsdb.partition_column='generation_time',
     tsdb.chunk_interval='1d',
-    tsdb.segmentedby='server_id'
+    tsdb.segmentby='server_id'
 );
 
-CALL add_columnstore_policy('server_application_logs', after => INTERVAL '1d');
+CALL add_columnstore_policy('servers_logs_application', after => INTERVAL '1d');
 
 CREATE TABLE servers_metrics(
     server_id varchar(255) NOT NULL,
@@ -62,10 +56,10 @@ CREATE TABLE servers_metrics(
     PRIMARY KEY (generation_time, server_id)
 )
 WITH(
-    tsdb.hypertable,
+    tsdb.hypertable=true,
     tsdb.partition_column='generation_time',
     tsdb.chunk_interval='1d',
-    tsdb.segmentedby='server_id'
+    tsdb.segmentby='server_id'
 );
 
 CALL add_columnstore_policy('servers_metrics', after => INTERVAL '1d');
