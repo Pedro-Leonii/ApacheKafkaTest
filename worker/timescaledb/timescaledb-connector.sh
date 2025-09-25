967407ec4f04327ed065d603dea83a8604212992
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-25 15:16:26 +0200

    feature(timescaledb worker): aggiunge configurazione del container woker per l'ingestion in timescaledb

---
+++ worker/timescaledb/timescaledb-connector.sh
@@ -0,0 +1,31 @@
+worker_addr=$1
+connection_password=$3
+connection_url="jdbc:postgresql://timescaledb:8888/kafka"
+connection_user="postgres"
+
+topics=(servers.logs.access servers.logs.application servers.metrics)
+
+for topic in ${topics[@]}; do 
+    connector="timescaledb-${topic//./-}"
+    curl -i -X PUT -H "Accept:application/json"\
+            -H "Content-Type:application/json" "http://$worker_addr:8083/connectors/$connector/config" \
+            -d  @- <<EOF
+{
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    
+    "tasks.max": 12,
+    "topics": "$topic",
+    
+    "connection.url": "$connection_url",
+    "connection.user": "$connection_user",
+    "connection.password": "$connection_password",
+
+    "db.timezone": "Europe/Rome",
+    "insert.mode": "upsert",
+    "pk.mode": "record_value",
+    "pk.fields": "generation_time, server_id",
+
+    "table.name.format": "${topic//./_}"
+}
+EOF
+done
