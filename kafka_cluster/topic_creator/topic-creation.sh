370c9a304908a9ee93446337853de7e064a6059f
Pedro Leoni <pedroleoni2001@gmail.com>
2025-09-30 10:40:30 +0200

    fix(topic-creator): definisce in un apposito docker compose

---
+++ kafka_cluster/topic_creator/topic-creation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+servers="broker1:9092,broker2:9093,broker3:9094"
+insync=2
+replication=3
+partition_n=12
+topics=(servers.metrics servers.logs.application servers.logs.access)
+
+until /opt/kafka/bin/kafka-topics.sh --bootstrap-server $servers --list; do
+  echo "Waiting for Kafka brokers..."
+  sleep 1
+done
+
+
+for topic in ${topics[@]}; do
+        /opt/kafka/bin/kafka-topics.sh --create \
+        --topic $topic \
+        --if-not-exists \
+        --partitions $partition_n \
+        --replication-factor $replication \
+        --config min.insync.replicas=$insync \
+        --bootstrap-server $servers
+done
+
+
+/opt/kafka/bin/kafka-topics.sh --create \
+        --topic _schemas \
+        --if-not-exists \
+        --partitions 1 \
+        --replication-factor $replication \
+        --config min.insync.replicas=$insync \
+        --config cleanup.policy=compact \
+        --bootstrap-server $servers
